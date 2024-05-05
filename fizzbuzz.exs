35cae2205462158eec1fec3812d0cc54625d8728
nvg <neglectedvalue@protonmail.com>
2024-05-05 10:11:36 +0300

    ut added

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,17 +1,46 @@
 defmodule Fizzbuzz do
 
   def main() do
-    Enum.each(1..100, &fizzbuzz/1)
+    1..100
+    |> Enum.map(&fizzbuzz/1) 
+    |> Enum.join(" ")
+    |> IO.puts()
   end
 
-def fizzbuzz(number) do
-  cond do
-    rem(number, 3) == 0 and rem(number, 5) == 0 -> IO.puts("FizzBuzz")
-    rem(number, 5) == 0 -> IO.puts("Buzz")
-    rem(number, 3) == 0 -> IO.puts("Fizz")
-    true -> IO.puts(number)
+  @spec fizzbuzz(integer()) :: String.t()
+  def fizzbuzz(number) do
+    
+    divisible_by_3 = rem(number, 3) == 0
+    divisible_by_5 = rem(number, 5) == 0
+
+    cond do
+      divisible_by_3 and divisible_by_5 -> "FizzBuzz"
+      divisible_by_5 -> "Buzz"
+      divisible_by_3 -> "Fizz"
+      true -> to_string(number)
+    end
   end
-end
 
 end
+
+ExUnit.start()
+
+defmodule FizzBuzzTest do
+  use ExUnit.Case
+  import Fizzbuzz
+
+  test "fizzbuzz 1-10" do
+    assert fizzbuzz(1) == "1"
+    assert fizzbuzz(2) == "2"
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(4) == "4"
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(6) == "Fizz"
+    assert fizzbuzz(7) == "7"
+    assert fizzbuzz(8) == "8"
+    assert fizzbuzz(9) == "Fizz"
+    assert fizzbuzz(10) == "Buzz"
+  end
+
+ end
 
