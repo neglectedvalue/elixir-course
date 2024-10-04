be2965c1c51dcacbf52799b147d60a7236a0ad6e
ngv <funnkycatz@yandex.ru>
2024-10-04 23:36:06 +0300

    small changes

--- my_calendar/lib/my_calendar.ex
+++ my_calendar/lib/my_calendar.ex
@@ -1,18 +1,7 @@
 defmodule MyCalendar do
-  @moduledoc """
-  Documentation for `MyCalendar`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MyCalendar.hello()
-      :world
-
-  """
   def hello do
     :world
   end
+
 end
+
